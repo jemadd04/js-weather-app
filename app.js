8e91c61538e4aafa6974628e13976c1f6abfc3d2
James Maddox <james.maddox07@gmail.com>
2020-02-06 11:08:40 -0500

    added celsius conversion

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ window.addEventListener('load', () => {
   );
   let temperatureDegree = document.querySelector('.temperature-degree');
   let locationTimezone = document.querySelector('.location-timezone');
+  let temperatureSection = document.querySelector('.temperature');
+  const temperatureSpan = document.querySelector('.temperature span');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -20,12 +22,34 @@ window.addEventListener('load', () => {
           return response.json();
         })
         .then(data => {
-          const { temperature, summary } = data.currently;
+          const { temperature, summary, icon } = data.currently;
           // Set DOM elements from API
-          temperatureDegree.textContent = temperature;
+          temperatureDegree.textContent = Math.floor(temperature);
           temperatureDescription.textContent = summary;
           locationTimezone.textContent = data.timezone;
+          // Celsius formula
+          let celsius = (temperature - 32) * (5 / 9);
+          // Set icon
+          setIcons(icon, document.querySelector('.icon'));
+
+          // Switch temp to celsius/faranheit
+          temperatureSection.addEventListener('click', () => {
+            if (temperatureSpan.textContent === 'F') {
+              temperatureSpan.textContent = 'C';
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent = 'F';
+              temperatureDegree.textContent = Math.floor(temperature);
+            }
+          });
         });
     });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: 'white' });
+    const currentIcon = icon.replace(/-/g, '_').toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
